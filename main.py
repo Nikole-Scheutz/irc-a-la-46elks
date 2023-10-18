ebba09a1a0d0ace4ced8a87050cccd3f92c14753
Nikole-Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
2023-10-18 09:54:35 +0200

    send simple SMS to the "nikole" user in users.json

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import json
+
+credentials_file = open("account_details.json")
+credentials = json.load(credentials_file)
+
+users_file = open("users.json")
+users_file = json.load(users_file)
+
+response = requests.post(
+        'https://api.46elks.com/a1/sms',
+        auth = (credentials["user_id"], credentials["user_secret"]),
+        data = {
+            'from': 'server',
+            'to': users_file["nikole"]["number"],
+            'message': 'you have a message!'
+            }
+        )
